254ed8dc8d3431ad09ec35f10bb0077fe31c458a
chenjiaxuu <249233507@qq.com>
2017-12-07 00:03:51 +0800

    修改vue目录header

---
+++ src/pages/home/header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <input type="text" v-model="msg">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+      msg: 'header'
+    }
+  }    
+}
+</script>
+
+
+<style scoped>
+  .hello {
+    background: pink;
+  }
+</style>
---
+++ src/pages/home/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <index-header />
+  </div>
+</template>
+
+<script>
+import header from './header.vue'
+export default {
+  components: {
+    "index-header": header
+  }
+}
+</script>
+
+
+<style scoped>
+  .hello {
+    background: red;
+  }
+</style>
